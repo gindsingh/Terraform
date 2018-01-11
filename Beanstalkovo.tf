8545774906912fc84d7f32762a779d878e948d68
gindsingh <gindsingh@gmail.com>
2018-01-11 15:42:47 -0800

    Add files via upload

---
+++ Beanstalkovo.tf
@@ -0,0 +1,131 @@
+
+resource "aws_elastic_beanstalk_environment" "ovo-microservice-example-uat" {
+  name = "ovo-microservice-example-uat"
+  application = "ovo-microservice-example"
+  solution_stack_name = "64bit Amazon Linux 2016.xxxx running Docker xxxx"
+  cname_prefix = "ovo-microservice-example-uat-domain"
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name = "IamInstanceProfile"
+    value = "aws-elasticbeanstalk-ec2-role"
+  }
+  setting {
+    namespace = "aws:ec2:vpc"
+    name = "VPCId"
+    value = "vpc-xxxxx"
+  }
+  setting {
+    namespace = "aws:ec2:vpc"
+    name = "AssociatePublicIpAddress"
+    value = "true"
+  }
+  setting {
+    namespace = "aws:ec2:vpc"
+    name = "Subnets"
+    value = "subnet-xxxx,subnet-xxxx,subnet-xxxxx"
+  }
+  setting {
+    namespace = "aws:ec2:vpc"
+    name = "ELBSubnets"
+    value = "subnet-xxxx,subnet-xxxx,subnet-xxxx"
+  }
+  setting {
+    namespace = "aws:ec2:vpc"
+    name = "ELBScheme"
+    value = "internal"
+  }
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name = "InstanceType"
+    value = "t2.micro"
+  }
+  setting {
+    namespace = "aws:autoscaling:asg"
+    name = "Availability Zones"
+    value = "Any 2"
+  }
+  setting {
+    namespace = "aws:autoscaling:asg"
+    name = "MinSize"
+    value = "2"
+  }
+  setting {
+    namespace = "aws:autoscaling:asg"
+    name = "MaxSize"
+    value = "3"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name = "ServiceRole"
+    value = "xxxx"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name = "environment"
+    value = "uat"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name = "LOGGING_APPENDER"
+    value = "GRAYLOG"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:healthreporting:system"
+    name = "SystemType"
+    value = "enhanced"
+  }
+  setting {
+    namespace = "aws:autoscaling:updatepolicy:rollingupdate"
+    name = "RollingUpdateEnabled"
+    value = "true"
+  }
+  setting {
+    namespace = "aws:autoscaling:updatepolicy:rollingupdate"
+    name = "RollingUpdateType"
+    value = "Health"
+  }
+  setting {
+    namespace = "aws:autoscaling:updatepolicy:rollingupdate"
+    name = "MinInstancesInService"
+    value = "2"
+  }
+  setting {
+    namespace = "aws:autoscaling:updatepolicy:rollingupdate"
+    name = "MaxBatchSize"
+    value = "1"
+  }
+  setting {
+    namespace = "aws:elb:loadbalancer"
+    name = "CrossZone"
+    value = "true"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:command"
+    name = "BatchSizeType"
+    value = "Fixed"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:command"
+    name = "BatchSize"
+    value = "1"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:command"
+    name = "DeploymentPolicy"
+    value = "Rolling"
+  }
+  setting {
+    namespace = "aws:elb:policies"
+    name = "ConnectionDrainingEnabled"
+    value = "true"
+  }
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name = "SecurityGroups"
+    value = "sg-xxxx"
+  }
+  tags {
+    Team = "OVO"
+    Environment = "UAT"
+  }
+}
---
+++ access.tf
@@ -0,0 +1,3 @@
+variable "aws_access_key" { default ="AKIAI4OWG4XPSPV6KOFQ"}
+variable "aws_secret_key" { default ="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"}
+variable "aws_region" {default="us-west-1"}
